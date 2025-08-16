a225cd03f1f8de3ce6dcd8b5103f6d00d5560045
Delacrux <ninocordova1611@gmail.comj>
2025-08-16 16:40:05 -0400

    Proyecto

---
+++ src/main/java/com/mycompany/proyectoescuela/ProyectoEscuela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoescuela;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ProyectoEscuela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
