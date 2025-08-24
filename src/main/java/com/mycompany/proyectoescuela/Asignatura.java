c8ff12da4d0a4842f0768204ca185da19c8a3c8b
Israel Pérez <>
2025-08-24 16:42:14 -0400

    Agregué las funcionalidades básicas de agregar y quitar, al igual que mostrar Dejé una función como comentario ya que depende de otra en Recursos digitales Tengo que hacer un toString en Nota :)

--- src/main/java/com/mycompany/proyectoescuela/Asignatura.java
+++ src/main/java/com/mycompany/proyectoescuela/Asignatura.java
@@ -9,46 +9,132 @@ public class Asignatura {
     private String nombreAsignatura;
     private ArrayList<RecursosDigitales> recursosDigitales;
     private HashMap<String, ArrayList<Nota>> notasAlumnos;
-    
-// --------------- Constructor  --------------- Ni idea que es wn xd
-    public Asignatura(Profesor profesorJefe, ArrayList<RecursosDigitales> recursosDigitales, HashMap<String, ArrayList<Nota>> notasAlumnos, String nombreAsignatura) {
+
+    // --------------- Constructor  ---------------
+    public Asignatura(Profesor profesorJefe, String nombreAsignatura) {
         this.profesorJefe = profesorJefe;
-        this.recursosDigitales = recursosDigitales;
-        this.notasAlumnos = notasAlumnos;
+        this.recursosDigitales = new ArrayList<>();
+        this.notasAlumnos = new HashMap<>();
         this.nombreAsignatura = nombreAsignatura;
     }
-    // --------------- Getter y Setter  --------------- 
+
+    // --------------- Métodos  ---------------
+    
     public String getNombreAsignatura() {
         return nombreAsignatura;
     }
-
     public void setNombreAsignatura(String nombreAsignatura) {
         this.nombreAsignatura = nombreAsignatura;
     }
-    
+
     public Profesor getProfesorJefe() {
         return profesorJefe;
     }
-
     public void setProfesorJefe(Profesor profesorJefe) {
         this.profesorJefe = profesorJefe;
     }
-
+    
+    //------Recursos Digitales------:
+    //Sobrecarga
     public ArrayList<RecursosDigitales> getRecursosDigitales() {
         return recursosDigitales;
     }
-
+    public RecursosDigitales getRecursosDigitales(int i){
+        if(i>=recursosDigitales.size() || i<0){return null;}
+        return recursosDigitales.get(i);
+    }
+    
+    //Sobrecarga: .
     public void setRecursosDigitales(ArrayList<RecursosDigitales> recursosDigitales) {
         this.recursosDigitales = recursosDigitales;
     }
+    public void setRecursosDigitales(RecursosDigitales recurso){
+        recursosDigitales.add(recurso);
+    }
+    
+    public boolean eliminarRecursosDigitales(RecursosDigitales recurso){
+        if(recursosDigitales.contains(recurso)){
+            recursosDigitales.remove(recurso);
+            return true;
+        }
+        return false;
+    }
+    
+    /*public void mostrarRecursosDigitales(){
+        int i;
+        for(i=0;i<recursosDigitales.size();i++){
+            System.out.println("Recurso n° "+ (i+1));
+            recursosDigitales.get(i).mostrarRecursoDigital();  <----//Hay una funcion acá que no existe aún, y la marcará como error sin el comentario.
+        }
+    }*/
 
+    //------NotasAlumnos------
     public HashMap<String, ArrayList<Nota>> getNotasAlumnos() {
         return notasAlumnos;
+    }
+    
+    public Nota getNotasAlumnos(String nombre, int i){
+        ArrayList<Nota> notas = notasAlumnos.get(nombre);
+        if(notas == null || notas.isEmpty() || i >= notas.size()){
+            return null;
+        }
+        return notas.get(i);
     }
     
     public void setNotasAlumnos(HashMap<String, ArrayList<Nota>> notasAlumnos) {
         this.notasAlumnos = notasAlumnos;
     }
     
+    public void agregarNotaAlumno(String nombre, Nota evaluacion) {
+        //Verificamos si hay una key con ese nombre
+        if(!notasAlumnos.containsKey(nombre)){
+            //No existe esta clave, así que la inicializamos
+            notasAlumnos.put(nombre, new ArrayList<>());
+        }
+        notasAlumnos.get(nombre).add(evaluacion);
+    }
     
+    public void quitarNotaAlumno(String nombre, Nota evaluacion) {
+        ArrayList<Nota> notas = notasAlumnos.get(nombre);
+        if (notas == null || !notas.contains(evaluacion)) {
+            System.out.println("Alumno no existe");
+            return;
+        }
+        notas.remove(evaluacion);
+    }
+   
+    //Sobrecarga
+    public void mostrarNotasAlumnos(String nombre){
+        ArrayList<Nota> notas = notasAlumnos.get(nombre);
+        //Verificamos si existe, retorna si no, imprime en caso de que sí.
+        if (notas == null) {
+            System.out.println("No existe el alumno " + nombre);
+            return;
+        }
+        int i = 1;
+        for (Nota nota : notas){
+            System.out.println((i)+") "+nota.toString());
+            i++;
+        }
+    }
+    public void mostrarNotasAlumnos(){
+        for(HashMap.Entry<String, ArrayList<Nota>> dupla: notasAlumnos.entrySet()){
+            String alumno = dupla.getKey();
+            ArrayList<Nota> notas = dupla.getValue();
+            int i = 1;
+            System.out.print("Alumno: "+alumno);
+            for(Nota nota : notas){
+                System.out.println(i+ ") " +nota.toString());
+                i++;
+            }
+        }
+    }
+
+    public void mostrarAsignatura(){
+        System.out.println("Asignatura");
+        System.out.println("Nombre: "+nombreAsignatura+". Profesor Jefe: "+ profesorJefe);
+        System.out.println("Cantidad de recursos: "+recursosDigitales.size());
+        System.out.println("Cantidad de alumnos: "+notasAlumnos.size());
+    }
+
 }
