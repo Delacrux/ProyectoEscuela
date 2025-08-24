20edad455a5547f59440b7c28137ca1139be4218
ferBarraza <fernandabarrazasoto@gmail.com>
2025-08-24 04:47:12 -0400

    Avance de main: Menpu alumnos e_e

--- src/main/java/com/mycompany/proyectoescuela/Sistema.java
+++ src/main/java/com/mycompany/proyectoescuela/Sistema.java
@@ -1,18 +1,21 @@
 package com.mycompany.proyectoescuela;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sistema {
     // --------------- Atributos  ---------------
     private ArrayList<Profesor> listaProfesores;
     private ArrayList<Alumno> listaAlumnos;
     private ArrayList<Curso> listaCursos;
+    private Scanner scanner;
     
     // --------------- Constructor  ---------------
     public Sistema() {
         this.listaProfesores = new ArrayList<Profesor>();
         this.listaAlumnos = new ArrayList<Alumno>();
         this.listaCursos = new ArrayList<Curso>();
+        this.scanner = new Scanner(System.in);
     }
     // --------------- Getter y Setter  ---------------
     public ArrayList<Profesor> getListaProfesores() {
@@ -80,24 +83,84 @@ public class Sistema {
     public void menuAlumno (Alumno alumno) {
         Curso cursoAlumno;
         cursoAlumno = buscarCurso(alumno.getCurso());
+        
         System.out.println("Bienvenido(a): " + alumno.getNombreApellido());
         System.out.println("Curso: " + cursoAlumno.getIdentificador());
         System.out.println();
+        System.out.println("Seleccione una asignatura para ver más detalles");
         mostrarAsignaturasCurso(cursoAlumno);
+        int numeroAsignatura = scanner.nextInt();
+        Asignatura materia;
+        materia = cursoAlumno.getAsignaturaPorID(numeroAsignatura);
+        
+        menuAsignatura(materia);
+    }
+    
+    public void menuAsignatura(Asignatura materia) {
+        int opcion;
+        do {
+            System.out.println("Asignatura: " + materia.getNombreAsignatura());
+            System.out.println();
+            System.out.println("Seleccione una opción");
+            System.out.println("1. Ver notas.");
+            System.out.println("2. Ver material digital");
+            System.out.println("3. Ver contacto profesor.");
+            System.out.println("4. Volver atrás");
+            System.out.println();
+        
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(opcion) {
+                case 1:
+                    //Ver notas del alumno en la asignatura xd
+                    break;
+                case 2:
+                    //Ver todo el material que hay en al asignatura
+                    break;
+                case 3:
+                    //Vwer datos de contacxto del profe, correo, telefono etc
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("No valido");
+            }
+        } while (opcion != 4);
     }
     
     public void menuInicial () {
         System.out.println("Bienvenido :p");
         System.out.println("Ingrese su rut. Ej: 123456789");
+        String rut = scanner.nextLine();
         
+        if (rut.equals("LeonardoCubillos")) {
+            menuAdministador();
+        }
+        else {
+            Alumno alumno;
+            alumno = buscarAlumno(rut);
+            if (alumno != null) {
+                menuAlumno(alumno);
+            }
+            else {
+                Profesor profesor;
+                profesor = buscarProfesor(rut);
+                if (profesor != null) {
+                    menuProfesor(profesor);
+                }
+                else {
+                    System.out.println("Rut no encontrado");
+                }
+            }
+        }
     }
     
     // ------------------------------------------------------------- MAIN  -------------------------------------------------------------
     // ------------------------------------------------------------- MAIN  -------------------------------------------------------------
     // ------------------------------------------------------------- MAIN  -------------------------------------------------------------
     public static void main(String[] args) {
-        
-     
-        
+       Sistema sistema = new Sistema();
+       sistema.menuInicial();
     }
 }
