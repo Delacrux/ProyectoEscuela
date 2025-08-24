d76d66285008783cb5cf1c3c3afe5ddfe7932755
ferBarraza <fernandabarrazasoto@gmail.com>
2025-08-24 03:17:14 -0400

    Recursos digitales :p

--- src/main/java/com/mycompany/proyectoescuela/RecursosDigitales.java
+++ src/main/java/com/mycompany/proyectoescuela/RecursosDigitales.java
@@ -7,8 +7,8 @@ public class RecursosDigitales {
     private String detalles;
     
     // --------------- Constructor  --------------
-    public RecursosDigitales(String tituloMaterial, String url, String detalles) {
-        this.tituloMaterial = tituloMaterial;
+    public RecursosDigitales(String titulo, String url, String detalles) {
+        this.tituloMaterial = titulo;
         this.url = url;
         this.detalles = detalles;
     }
@@ -16,27 +16,30 @@ public class RecursosDigitales {
     public String getTituloMaterial() {
         return tituloMaterial;
     }
-
-    public void setTituloMaterial(String tituloMaterial) {
-        this.tituloMaterial = tituloMaterial;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getDetalles() {
         return detalles;
     }
-
+    public String getUrl() {
+        return url;
+    }
+    
+    public void setTituloMaterial(String tituloMaterial) {
+        this.tituloMaterial = tituloMaterial;
+    }
     public void setDetalles(String detalles) {
         this.detalles = detalles;
     }
+    public void setUrl(String url) {
+        this.url = url;
+    }
     
+    public void imprimirRecursoDigital() {
+        System.out.println(" =============================== ");
+        System.out.println(" Título: " + this.tituloMaterial);
+        System.out.println("URL: " + this.url);
+        System.out.println("Detalles: " + this.detalles);
+        System.out.println(" =============================== ");
+    }
     
     
     
