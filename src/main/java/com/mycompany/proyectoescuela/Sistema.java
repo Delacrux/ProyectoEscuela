4c2e5bc226866dcaba6dabd74b066b93578b10f1
ferBarraza <fernandabarrazasoto@gmail.com>
2025-08-25 05:43:16 -0400

    Avance de main: Menpu alumnos e_e

--- src/main/java/com/mycompany/proyectoescuela/Sistema.java
+++ src/main/java/com/mycompany/proyectoescuela/Sistema.java
@@ -72,31 +72,39 @@ public class Sistema {
         return null;
     }
     
-    public void mostrarAsignaturasCurso(Curso curso) {
+    public void mostrarAsignaturasMenu(ArrayList<Asignatura> asignaturas) {
         int i;
-        ArrayList<Asignatura> asignaturas = curso.getListaAsignaturas();
         for (i = 0 ; i < asignaturas.size() ; i++) {
             System.out.println((i + 1) + ". " + asignaturas.get(i).getNombreAsignatura());
         }
     }
     
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    
     public void menuAlumno (Alumno alumno) {
-        Curso cursoAlumno;
-        cursoAlumno = buscarCurso(alumno.getCurso());
+        Curso cursoAlumno = buscarCurso(alumno.getCurso());
+        ArrayList<Asignatura> asignaturasCurso = cursoAlumno.getListaAsignaturas();
         
         System.out.println("Bienvenido(a): " + alumno.getNombreApellido());
         System.out.println("Curso: " + cursoAlumno.getIdentificador());
         System.out.println();
         System.out.println("Seleccione una asignatura para ver más detalles");
-        mostrarAsignaturasCurso(cursoAlumno);
-        int numeroAsignatura = scanner.nextInt();
-        Asignatura materia;
-        materia = cursoAlumno.getAsignaturaPorID(numeroAsignatura);
+        mostrarAsignaturasMenu(asignaturasCurso);
         
-        menuAsignatura(materia);
+        int numeroAsignatura = scanner.nextInt();
+        Asignatura materia = cursoAlumno.getAsignaturaPorMenu(numeroAsignatura);
+        /* mostrarAsignaturasCurso muetra el arrayList de asignaturas que tiene el curso, se muestran en el orden en 
+        se ingresaron las asignaturas y se seleccionar la asignatura por la posición
+        Indice         0  1   2   3
+        Posicion     1  2   3   4
+        entonces ahora con getAsignaturaPorMenu, el parametro es solo el número de posicion de la asignatur que se quiere obtener,
+        entonces solo hay que retornar la asignatura[posicion -1] para poder acceder a la que se quiere xd*/
+        menuAsignaturaAlumno(materia, alumno);
     }
     
-    public void menuAsignatura(Asignatura materia) {
+    public void menuAsignaturaAlumno(Asignatura materia, Alumno alumno) {
         int opcion;
         do {
             System.out.println("Asignatura: " + materia.getNombreAsignatura());
@@ -113,22 +121,83 @@ public class Sistema {
 
             switch(opcion) {
                 case 1:
-                    //Ver notas del alumno en la asignatura xd
+                    //Ver notas del alumno en la asignatura 
                     break;
                 case 2:
                     //Ver todo el material que hay en al asignatura
                     break;
                 case 3:
-                    //Vwer datos de contacxto del profe, correo, telefono etc
+                    //Ver datos de contacxto del profe encargado de la asignatura, como correo, telefono etc
                     break;
                 case 4:
                     break;
                 default:
-                    System.out.println("No valido");
+                    System.out.println("Opción no válida");
             }
         } while (opcion != 4);
     }
     
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    
+    public void menuProfesor(Profesor profesor) {
+        ArrayList<Asignatura> listaAsignaturas = profesor.get______();
+        System.out.println("Bienvenido(a): " + profesor.getNombreApellido());
+        System.out.println();
+        System.out.println("Seleccione una asignatura para ver más detalles");
+        mostrarAsignaturasMenu(listaAsignaturas);
+        
+        int numeroAsignatura = scanner.nextInt();
+        Asignatura materia = listaAsignaturas.getAsignaturaPorMenu(numeroAsignatura);
+        
+        menuAsignaturaProfesor(materia);
+    }
+    
+    public void menuAsignaturaProfesor(Asignatura materia) {
+        int opcion;
+        do{
+            System.out.println("Asignatura: " + materia.getNombreAsignatura());
+            System.out.println();
+            System.out.println("Seleccione una opción");
+            System.out.println("1. Ver lista alumnos.");
+            System.out.println("2. Ver notas de alumnos.");
+            System.out.println("3. Ver material digital.");
+            System.out.println("4. Agregar nota a alumno.");
+            System.out.println("5. Agregar material digital.");
+            System.out.println("6. Volver atrás.");
+            System.out.println();
+        
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch(opcion) {
+                case 1:
+                    //Ver la lista de los alumnos de la asignatura
+                    break;
+                case 2:
+                    //Ver las notas de los alumnos en la asignatura
+                    break;
+                case 3:
+                    // Ver todo el amterial digital que hay en la asignatura 
+                    break;
+                case 4:
+                    //Agregar una nota a un alumno en la asignatura
+                    break;
+                case 5:
+                    //Agregar meterial digital xd
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        }while(opcion != 6);
+    }
+    
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
+    // ------------------------------------------------------------- MENU  -------------------------------------------------------------
     public void menuInicial () {
         System.out.println("Bienvenido :p");
         System.out.println("Ingrese su rut. Ej: 123456789");
@@ -163,4 +232,5 @@ public class Sistema {
        Sistema sistema = new Sistema();
        sistema.menuInicial();
     }
+    // XD medio main SKDJSKDJ
 }
