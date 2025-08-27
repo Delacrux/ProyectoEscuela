f79d5f44b4981c14bd7427ab580bb49c95b7e7e5
Israel Pérez <AzukZ@LAP-IS>
2025-08-27 12:29:17 -0400

    Cambié el HashMap a contener Alumno como llave. Asimismo, adapté las funciones para que tengan sentido con el cambio. Hice algun que otro cambio para mejor comprensión.

--- src/main/java/com/mycompany/proyectoescuela/Asignatura.java
+++ src/main/java/com/mycompany/proyectoescuela/Asignatura.java
@@ -8,7 +8,7 @@ public class Asignatura {
     private Profesor profesorJefe;
     private String nombreAsignatura;
     private ArrayList<RecursosDigitales> recursosDigitales;
-    private HashMap<String, ArrayList<Nota>> notasAlumnos;
+    private HashMap<Alumno, ArrayList<Nota>> notasAlumnos;
 
     // --------------- Constructor  ---------------
     public Asignatura(Profesor profesorJefe, String nombreAsignatura) {
@@ -19,10 +19,11 @@ public class Asignatura {
     }
 
     // --------------- getter y setter  ---------------
-    
+
     public String getNombreAsignatura() {
         return nombreAsignatura;
     }
+
     public void setNombreAsignatura(String nombreAsignatura) {
         this.nombreAsignatura = nombreAsignatura;
     }
@@ -30,10 +31,11 @@ public class Asignatura {
     public Profesor getProfesorJefe() {
         return profesorJefe;
     }
+
     public void setProfesorJefe(Profesor profesorJefe) {
         this.profesorJefe = profesorJefe;
     }
-    
+
     public ArrayList<RecursosDigitales> getRecursosDigitales() {
         return recursosDigitales;
     }
@@ -41,33 +43,36 @@ public class Asignatura {
         if(i>=recursosDigitales.size() || i<0){return null;}
         return recursosDigitales.get(i);
     }
-    
+
     public void setRecursosDigitales(ArrayList<RecursosDigitales> recursosDigitales) {
         this.recursosDigitales = recursosDigitales;
     }
-    
-    public HashMap<String, ArrayList<Nota>> getNotasAlumnos() {
+
+    public HashMap<Alumno, ArrayList<Nota>> getNotasAlumnos() {
         return notasAlumnos;
     }
-    public Nota getNotasAlumnos(String nombre, int i){
-        ArrayList<Nota> notas = notasAlumnos.get(nombre);
+
+    public Nota getNotasAlumnos(Alumno estudiante, int i){
+        ArrayList<Nota> notas = notasAlumnos.get(estudiante);
+
         if(notas == null || notas.isEmpty() || i >= notas.size()){
+            System.out.println("Nota no encontrada en índice "+ i);
             return null;
         }
         return notas.get(i);
     }
-    
-    public void setNotasAlumnos(HashMap<String, ArrayList<Nota>> notasAlumnos) {
+
+    public void setNotasAlumnos(HashMap<Alumno, ArrayList<Nota>> notasAlumnos) {
         this.notasAlumnos = notasAlumnos;
     }
 
-    
+
     //Métodos.
-    
+
     public void agregarRecursoDigital(RecursosDigitales recurso){
         recursosDigitales.add(recurso);
     }
-    
+
     public boolean eliminarRecursoDigital(RecursosDigitales recurso){
         if(recursosDigitales.contains(recurso)){
             recursosDigitales.remove(recurso);
@@ -75,7 +80,7 @@ public class Asignatura {
         }
         return false;
     }
-    
+
     public void mostrarRecursosDigitales(){
         int i;
         for(i=0;i<recursosDigitales.size();i++){
@@ -85,53 +90,84 @@ public class Asignatura {
     }
 
     //------NotasAlumnos------
-    public void agregarNotaAlumno(String nombre, Nota evaluacion) {
+    public void agregarNotaAlumno(Alumno estudiante, Nota evaluacion) {
         //Verificamos si hay una key con ese nombre
-        if(!notasAlumnos.containsKey(nombre)){
+        if(!notasAlumnos.containsKey(estudiante)){
             //No existe esta clave, así que la inicializamos
-            notasAlumnos.put(nombre, new ArrayList<>());
+            notasAlumnos.put(estudiante, new ArrayList<>());
         }
-        notasAlumnos.get(nombre).add(evaluacion);
+        notasAlumnos.get(estudiante).add(evaluacion);
     }
-    public void eliminarNotaAlumno(String nombre, Nota evaluacion) {
-        ArrayList<Nota> notas = notasAlumnos.get(nombre);
-        if (notas == null || !notas.contains(evaluacion)) {
+
+    public void eliminarNotaAlumno(Alumno estudiante, Nota evaluacion) {
+        ArrayList<Nota> notas = notasAlumnos.get(estudiante);
+        if (notas == null) {
             System.out.println("Alumno no existe");
+            return;
+        }
+        if(!notas.contains(evaluacion)){
+            System.out.println("No existe esta nota para el Alumno");
             return;
         }
         notas.remove(evaluacion);
     }
-   
+
     //Sobrecarga
-    public void mostrarNotasAlumnos(String nombre){
-        ArrayList<Nota> notas = notasAlumnos.get(nombre);
+    public void mostrarNotasAlumnos(Alumno estudiante){
+        ArrayList<Nota> notas = notasAlumnos.get(estudiante);
+
         //Verificamos si existe, retorna si no, imprime en caso de que sí.
         if (notas == null) {
-            System.out.println("No existe el alumno " + nombre);
+            System.out.println("No existe alumno " + estudiante.getNombreApellido());
             return;
         }
+        
+        if(notas.isEmpty()){
+            System.out.println("Alumno " + estudiante.getNombreApellido() + " no tiene notas");
+            return;
+        }
+        
         int i = 1;
         for (Nota nota : notas){
-            System.out.println((i)+") "+nota.toString());
+            System.out.println((i)+") "+nota.getNota() + " " + nota.getDetalleNota());
             i++;
         }
     }
     public void mostrarNotasAlumnos(){
-        for(HashMap.Entry<String, ArrayList<Nota>> dupla: notasAlumnos.entrySet()){
-            String alumno = dupla.getKey();
+        for(HashMap.Entry<Alumno, ArrayList<Nota>> dupla: notasAlumnos.entrySet()){
+            Alumno alumno = dupla.getKey();
             ArrayList<Nota> notas = dupla.getValue();
             int i = 1;
-            System.out.print("Alumno: "+alumno);
-            for(Nota nota : notas){
-                System.out.println(i+ ") " +nota.toString());
-                i++;
+
+            System.out.println("Alumno: "+alumno.getNombreApellido());
+            
+            if(notas.isEmpty()){
+            System.out.println("Alumno " + alumno.getNombreApellido() + " no tiene notas");
             }
+            
+            else{
+                for(Nota nota : notas){
+                    System.out.println(i+ ") " +nota.getNota());
+                    i++;
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public void mostrarAlumnos(){
+        int i  = 1;
+        for(HashMap.Entry<Alumno, ArrayList<Nota>> dupla : notasAlumnos.entrySet()){
+            Alumno alumno = dupla.getKey();
+
+            System.out.println((i) + ") Alumno: "+ alumno.getNombreApellido() + " " + alumno.getRut());
+            i++;
         }
     }
 
     public void mostrarAsignatura(){
         System.out.println("Asignatura");
-        System.out.println("Nombre: "+nombreAsignatura+". Profesor Jefe: "+ profesorJefe);
+        System.out.println("Nombre: "+nombreAsignatura+". Profesor Jefe: "+ profesorJefe.getNombreApellido());
         System.out.println("Cantidad de recursos: "+recursosDigitales.size());
         System.out.println("Cantidad de alumnos: "+notasAlumnos.size());
     }
