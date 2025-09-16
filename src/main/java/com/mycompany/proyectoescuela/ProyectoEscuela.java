1323b13ce946eadf5f2ed740d4d3b854388fa16e
ferBarraza <fernandabarrazasoto@gmail.com>
2025-09-16 04:23:33 -0300

    Agregué métodos en ProyectoEscuela Agregar, Eliminar y Mostrar para Alumno, Profesor y Curso... Con sus respectivas gestiones y por último ordené un poco el código.
    (Ya está la base de los métodos para agregar y eliminar. Todavía falta la lógica para actualizar las referencias en otras clases, como por ejemplo, sacar a un alumno de su curso cuando es eliminadosss)

--- src/main/java/com/mycompany/proyectoescuela/ProyectoEscuela.java
+++ src/main/java/com/mycompany/proyectoescuela/ProyectoEscuela.java
@@ -1,12 +1,12 @@
 package com.mycompany.proyectoescuela;
 
 import java.util.Set;
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+// ----------- Clase ProyectoEscuela -----------
 public class ProyectoEscuela {
     // ------------------------------------------------------------- Atributos  -------------------------------------------------------------
     private ArrayList<Profesor> listaProfesores;
@@ -48,7 +48,7 @@ public class ProyectoEscuela {
 
     // ------------------------------------------------------------- Métodos  -------------------------------------------------------------
     
-     // ------------------------------  Búsqueda ------------------------------ 
+    // ------------------------------  BUSQUEDA  ------------------------------ 
     public Alumno buscarAlumnoSistema (String rut) {
         int i;
         for(i = 0 ; i < listaAlumnos.size() ; i++) {
@@ -79,11 +79,207 @@ public class ProyectoEscuela {
         return null;
     }
     
-    // ------------------------------  Mostrar ------------------------------ 
+    // ------------------------------  AGREGAR  ------------------------------ 
+    public Profesor crearYAgregarProfesor(String rut, String nombreApellido, String especialidad, String correo, String telefono) {
+        if (buscarProfesorSistema(rut) == null ) {
+            Profesor nuevoProfesor =new Profesor(rut, nombreApellido, especialidad, correo, telefono);
+            listaProfesores.add(nuevoProfesor);
+            return nuevoProfesor;
+        }
+        return null;
+    }
+   
+    public Alumno crearYAgregarAlumno(String rut, String nombreApellido, String correo, String telefono, String curso) {
+        Curso cursoAlumno = buscarCursoSistema(curso);
+        if (buscarAlumnoSistema(rut) == null && cursoAlumno != null) {
+            Alumno nuevoAlumno = new Alumno(rut, nombreApellido, correo, telefono, cursoAlumno);
+            listaAlumnos.add(nuevoAlumno);
+            return nuevoAlumno;
+        }
+        return null;
+    }
+    
+    public Curso crearYAgregarCurso(String rutProfeJefe, String identificador) {
+        Profesor profesorJefe = buscarProfesorSistema(rutProfeJefe);
+        
+        if (buscarCursoSistema(identificador) == null && profesorJefe != null) {
+            Curso nuevoCurso = new Curso(profesorJefe, identificador);
+            listaCursos.add(nuevoCurso);
+            return nuevoCurso;
+        }
+        return null;
+    }
+    
+    //----------------- GESTIONAR AGREGAR --------------------  
+    public void gestionarCrearProfesor() {
+        System.out.println("Rutprofe");
+        String rut =  scanner.nextLine();
+        System.out.println("nombre profe");
+        String nombreApellido = scanner.nextLine();
+        System.out.println("especialidad");
+        String especialidad = scanner.nextLine();
+        System.out.println("correoo");
+        String correo = scanner.nextLine();
+        System.out.println("telefono");
+        String telefono = scanner.nextLine();
+        
+        if (crearYAgregarProfesor(rut, nombreApellido, especialidad, correo, telefono) == null) {
+            System.out.println("falló");
+        }
+        else {
+            System.out.println("profesor creado y agregado con exito");
+        }
+    }
+    
+    public void gestionarCrearAlumno() {
+        System.out.println("rut alumno");
+        String rut =  scanner.nextLine();
+        System.out.println("nombre alumno");
+        String nombreApellido = scanner.nextLine();
+        System.out.println("correo  alumno");
+        String correo = scanner.nextLine();
+        System.out.println("telefono  alumno");
+        String telefono = scanner.nextLine();
+        System.out.println("curso del alumno");
+        String curso = scanner.nextLine();
+        
+        if (crearYAgregarAlumno(rut, nombreApellido, correo, telefono, curso) == null) {
+            System.out.println("falló");
+        }
+        else {
+            System.out.println("Alumno creado y agregado con exito");
+        }
+    }
+    
+    public void gestionarCrearCurso() {
+        System.out.println("Identificador del curso");
+        String identificador = scanner.nextLine();
+        System.out.println("nombre del profe jefe");
+        String rutProfeJefe = scanner.nextLine();
+        
+        if (crearYAgregarCurso(rutProfeJefe, identificador) == null) {
+            System.out.println("falló");
+        }
+        else {
+            System.out.println("curso creado con exito");
+        }
+    }
+    
+    // ------------------------------  ELIMINAR  ------------------------------ 
+    public Profesor eliminarProfesor(String rutProfesor) {
+        Profesor profesor = buscarProfesorSistema(rutProfesor);
+        if (profesor != null) {
+            listaProfesores.remove(profesor);
+            return profesor;
+        }
+        return null;
+    }
+    
+    public Alumno eliminarAlumno(String rutAlumno) {
+        Alumno alumno = buscarAlumnoSistema(rutAlumno);
+        if (alumno != null) {
+            listaAlumnos.remove(alumno);
+            return alumno;
+        }
+        return null;
+    }
+    
+    public Curso eliminarCurso(String identificador) {
+        Curso curso = buscarCursoSistema(identificador);
+        if (curso != null) {
+            listaCursos.remove(curso);
+            return curso;
+        }
+        return null;
+    }
+    
+    //----------------- GESTIONAR ELIMINAR  --------------------  
+    public void gestionarEliminarProfesor() {
+        System.out.println("Rut del profe a eliminar");
+        String rut = scanner.nextLine();
+        
+        if (eliminarProfesor(rut) == null) {
+            System.out.println("falló");
+        }
+        else {
+            System.out.println("Profesor eliminado con exito.");
+        }
+    }
+    
+    public void gestionarEliminarAlumno() {
+        System.out.println("Rut del Alumno a eliminar");
+        String rut = scanner.nextLine();
+        
+        if (eliminarAlumno(rut) == null) {
+            System.out.println("falló");
+        }
+        else {
+            System.out.println("Alumno eliminado con exito.");
+        }
+    }
+    
+    public void gestionarEliminarCurso() {
+        System.out.println("Identificador del curso a eliminar");
+        String identificador = scanner.nextLine();
+        
+        if (eliminarCurso(identificador) == null) {
+            System.out.println("falló");
+        }
+        else {
+            System.out.println("Curso eliminado con exito.");
+        }
+    }
+    
+    // ------------------------------  MOSTRAR   ------------------------------ 
     public void mostrarCursosMenu() {
         int i;
         for (i = 0 ; i < listaCursos.size() ; i++) {
             System.out.println((i+1) + ". " + listaCursos.get(i).getIdentificador());
+        }
+    }
+    
+    public ArrayList <Curso> mostrarCursosProfesor(Profesor profesor) {
+        Set<Curso> lista = profesor.getCursosAsignados();
+        int i = 1;
+        for (Curso curso : lista) {
+            System.out.println(i + ". " + curso.getIdentificador());
+            i++;
+        }
+        ArrayList<Curso> arraylist = new ArrayList<>(lista);
+        return arraylist;
+    }
+    
+    public ArrayList<String> mostrarAsignaturasProfesor(Profesor profesor, Curso curso) {
+        ArrayList<String> lista = profesor.getAsignaturasEnCurso(curso);
+        for (int i = 0 ; i < lista.size() ; i++) {
+            String asignatura = lista.get(i);
+            System.out.println((i + 1) + ". " + asignatura);
+        }
+        return lista;
+    }
+    
+    // ------------------------------  MOSTRAR SISTEMA  ------------------------------ 
+    
+    public void mostrarCursosSistema() {
+        System.out.println("Cantidad de cursos registrados en el sistema: " + listaCursos.size());
+        for (int i = 0 ; i < listaCursos.size() ; i++) {
+            System.out.println((i + 1) + ". " +listaCursos.get(i).getIdentificador());
+        }
+    }
+    
+    public void mostrarProfesoresSistema() {
+        System.out.println("Cantidad de profesores registrados en el sistema: " + listaProfesores.size());
+        for (int i = 0 ; i < listaProfesores.size() ; i++) {
+            Profesor profesor = listaProfesores.get(i);
+            System.out.println((i + 1) + ". " + profesor.nombreApellido + " - " + profesor.rut );
+        }
+    }
+    
+    public void mostrarAlumnosSistema() {
+        System.out.println("Cantidad de alumnos registrados en el sistema: " + listaAlumnos.size());
+        for (int i = 0 ; i < listaCursos.size() ; i++) {
+            Alumno alumno = listaAlumnos.get(i);
+            System.out.println((i + 1) + ". " + alumno.nombreApellido + " - " + alumno.rut );
         }
     }
     
@@ -102,13 +298,12 @@ public class ProyectoEscuela {
        RecursoDigital recurso = new RecursoDigital(titulo, url, detalle);
    }
    
-       //------------------------------Datos Iniciales ------------------------------
+       //------------------------------ DATOS INICIALES  ------------------------------
     public void llenarDatos() {
         System.out.println("Cargando datos...");
         llenadoDeProfesores();
         llenadoDeCursos();
         llenadoDeAlumnos();
-        
         System.out.println("Datos cargados correctamente");
     }
     
@@ -164,6 +359,7 @@ public class ProyectoEscuela {
             System.out.println("Error al leer Alumnos.csv: " + e.getMessage());
         }
     }
+    
     public void llenadoDeCursos(){
         try(Scanner sc  = new  Scanner(new File("data/alumnos.csv"))){
             while(sc.hasNextLine()){
@@ -205,9 +401,7 @@ public class ProyectoEscuela {
         
         menuAsignaturaAlumno(materia, alumno);
     }
-    
      // ------------------------------------------------------------- MENU ALUMNO -------------------------------------------------------------
-    
     public void menuAsignaturaAlumno(Asignatura materia, Alumno alumno) {
         int opcion;
         do {
@@ -241,35 +435,10 @@ public class ProyectoEscuela {
             }
         } while (opcion != 4);
     }
-    */
+  
     // ------------------------------------------------------------- MENU PROFESOR -------------------------------------------------------------
     // ------------------------------------------------------------- MENU PROFESOR -------------------------------------------------------------
-    // ------------------------------------------------------------- MENU PROFESOR -------------------------------------------------------------
-    public ArrayList <Curso> mostrarCursosProfesor(Profesor profesor) {
-        Set<Curso> lista = profesor.getCursosAsignados();
-        int i = 1;
-        for (Curso curso : lista) {
-            System.out.println(i + ". " + curso.getIdentificador());
-            i++;
-        }
-        ArrayList<Curso> arraylist = new ArrayList<>(lista);
-        return arraylist;
-    }
-    
-    public ArrayList<String> mostrarAsignaturas(Profesor profesor, Curso curso) {
-        ArrayList<String> lista = profesor.getAsignaturasEnCurso(curso);
-        for (int i = 0 ; i < lista.size() ; i++) {
-            String asignatura = lista.get(i);
-            System.out.println((i + 1) + ". " + asignatura);
-        }
-        return lista;
-    }
-    
-    /*
-    public ArrayList<String> mostrarAsignaturas ( Curso curso) {
-    
-    } */
-    
+    // ------------------------------------------------------------- MENU PROFESOR -----------------------------------------------------------
     public void menuProfesor(Profesor profesor) {
         System.out.println("Bienvenido(a): " + profesor.getNombreApellido());
         System.out.println();
@@ -325,8 +494,7 @@ public class ProyectoEscuela {
             }
         }while(opcion != 3);
     }
-    
-    
+    */
     // ------------------------------------------------------------- LOGIN -------------------------------------------------------------
     // ------------------------------------------------------------- LOGIN -------------------------------------------------------------
     // ------------------------------------------------------------- LOGIN -------------------------------------------------------------
@@ -336,19 +504,24 @@ public class ProyectoEscuela {
         System.out.println("Ingrese su rut. Ej: 123456789");
         String rut = scanner.nextLine();
         
-        Alumno alumno;
-        alumno = buscarAlumnoSistema(rut);
-        if (alumno != null)  {
-            menuAlumno(alumno);
+        if (rut.equals("Admin")) {
+            //menuAdministador();
         }
         else {
-            Profesor profesor;
-            profesor = buscarProfesorSistema(rut);
-            if (profesor != null) {
-                menuProfesor(profesor);
+            Alumno alumno;
+            alumno = buscarAlumnoSistema(rut);
+            if (alumno != null) {
+                //menuAlumno(alumno);
             }
             else {
-                System.out.println("Rut no encontrado");
+                Profesor profesor;
+                profesor = buscarProfesorSistema(rut);
+                if (profesor != null) {
+                    //menuProfesor(profesor);
+                }
+                else {
+                    System.out.println("Rut no encontrado");
+                }
             }
         }
     }
