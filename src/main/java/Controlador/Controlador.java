47203496e766dd93131b134457ee148bd9dba372
Israel Pérez <AzukZ@LAP-IS>
2025-09-27 19:29:04 -0300

    Cerrar login en ventanaAdmin ggg

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -49,6 +49,7 @@ public class Controlador {
          System.out.println("Login comenzado con exito");
          
          if(rut.equals("20878643")){
+             ventanaLogin.dispose();
              new MenuAdmin(this, sistema.getListaCursos(), sistema.getListaProfesores()).setVisible(true);
          }
          
